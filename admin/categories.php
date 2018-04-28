e8d8ded6c0ba8c92b581288f01c756748099868b
Han_Codes <hanniek55@gmail.com>
2018-04-28 18:17:08 -0500

    Created function findAllCategories and deleteCategories

--- admin/categories.php
+++ admin/categories.php
@@ -43,31 +43,9 @@
                           </thead>
                           <tbody>
 
-                              <?php // Find All Categories Query
-                              $query = "SELECT * FROM categories";
-                              $select_categories = mysqli_query($connection, $query);
+                              <?php findAllCategories(); ?>
 
-                              while($row = mysqli_fetch_assoc($select_categories)) {
-                                $cat_id = $row['cat_id'];
-                                $cat_title = $row['cat_title'];
-                                echo "<tr>";
-                                echo "<td>{$cat_id}</td>";
-                                echo "<td>{$cat_title}</td>";
-                                echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                echo "</tr>";
-                              }
-
-                               ?>
-
-                               <?php // DELETE QUERY
-                               if(isset($_GET['delete'])) {
-                                 $the_cat_id = $_GET['delete'];
-                                 $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-                                 $delete_query = mysqli_query($connection, $query);
-                                 header("Location: categories.php"); // will refresh the page
-                               }
-                                ?>
+                               <?php deleteCategories(); ?>
                             </tbody>
                           </table>
                         </div>
