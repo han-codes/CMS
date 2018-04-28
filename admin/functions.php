4e57d80ee22fd0e7200cfba5251b9f0dc3c92053
Han_Codes <hanniek55@gmail.com>
2018-04-28 10:21:53 -0500

    including function to insert categories into the database

---
+++ admin/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+
+  function insert_categories() {
+    if(isset($_POST['submit'])) {
+      $cat_title = $_POST['cat_title'];
+
+      if($cat_title == "" || empty($cat_title)) {
+        echo "This field should not be empty.";
+      } else {
+        $query = "INSERT INTO categories(cat_title) ";
+        $query .= "VALUES('{$cat_title}') ";
+
+        $create_category_query = mysqli_query($connection, $query);
+        if(!$create_category_query) {
+          die("Query Failed." . mysqli_error($connectioN));
+        }
+      }
+    }
+  }
+ ?>
