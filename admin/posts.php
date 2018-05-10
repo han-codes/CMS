153acde6050e3f14a5e9fa092acd588fff42cd02
Han_Codes <hanniek55@gmail.com>
2018-05-10 17:37:15 +0900

    create table

--- admin/posts.php
+++ admin/posts.php
@@ -14,6 +14,34 @@
                           Welcome to Admin
                           <small>Author</small>
                       </h1>
+                      <table class="table table-bordered table-hover">
+                        <thead>
+                          <tr>
+                            <th>Id</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>10</td>
+                            <td>Edwin Diaz</td>
+                            <td>Bootstrap Framework </td>
+                            <td>Bootstrap</td>
+                            <td>Status</td>
+                            <td>Image</td>
+                            <td>Tags</td>
+                            <td>Comments</td>
+                            <td>Date</td>
+                          </tr>
+                        </tbody>
+                      </table>
                     </div>
 
                     </div>
