68f61aab1b5907a696fed51b9427cd4a72902d89
Han_Codes <hanniek55@gmail.com>
2018-05-14 19:42:07 +0900

    Set inputs to a php variable

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,6 +1,18 @@
 <?php
 if (isset($_POST['create_post'])) {
-  echo $_POST['title'];
+  $post_title = $_POST['title'];
+  $post_author = $_POST['author'];
+  $post_category_id = $_POST['post_category_id'];
+  $post_status = $_POST['post_status'];
+
+  $post_image = $_FILES['image']['name'];
+  // to hold our temporary file name
+  $post_image_temp = $_FILES['image']['tmp_name'];
+
+  $post_tags = $_POST['post_tags'];
+  $post_content = $_POST['post_content'];
+  $post_date = date('d-m-y');
+  $post_comment_count = 4;
 }
  ?>
 
