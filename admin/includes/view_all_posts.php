3313e44d3abd0986396c5e279b8fb4b010746c46
Han_Codes <hanniek55@gmail.com>
2018-05-18 10:51:37 +0900

    add the delete link and create the query to delete the user specified row from database

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -47,8 +47,10 @@
      ?>
 
      <?php
-     if (isset($_GET['delete']) {
-       
+     if (isset($_GET['delete'])) {
+       $the_post_id = $_GET['delete'];
+       $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
+       $delete_query = mysqli_query($connection, $query);
      }
 
       ?>
