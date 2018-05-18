ecad36325140789c7498b0a547d4a8e3b3649ead
Han_Codes <hanniek55@gmail.com>
2018-05-18 15:09:43 +0900

    Dynamic <option> for <select> categories

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -26,8 +26,21 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
   </div>
 
   <div class="form-group">
-    <label for="post_category">Post Category</label>
-    <input type="text" name="post_category_id" class="form-control" value="<?php echo $post_category_id; ?>">
+    <select class="" name="">
+      <?php
+      $query = "SELECT * FROM categories";
+      $select_categories = mysqli_query($connection, $query);
+      confirmQuery($select_categories);
+
+      while($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+
+        echo "<option value='{$cat_id}'>{$cat_title}</option>";
+      }
+      ?>
+
+    </select>
   </div>
 
   <div class="form-group">
