e8d8cee22ec9bc2753cc4308862914cfb12a244d
Han_Codes <hanniek55@gmail.com>
2018-05-18 12:42:02 +0900

    add the dynamic value to input boxes

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,5 +1,7 @@
 <?php
-
+if (isset($_GET['p_id'])){
+  $the_post_id =  $_GET['p_id'];
+}
 $query = "SELECT * FROM posts";
 $select_posts_by_id = mysqli_query($connection, $query);
 
@@ -10,31 +12,32 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
   $post_category_id = $row['post_category_id'];
   $post_status = $row['post_status'];
   $post_image = $row['post_image'];
+  $post_content = $row['post_content'];
   $post_tags = $row['post_tags'];
   $post_comment_count = $row['post_comment_count'];
   $post_date = $row['post_date'];
-
+}
  ?>
 
 <form action="" method="post" enctype="multipart/form-data">
   <div class="form-group">
     <label for="title">Post Title</label>
-    <input type="text" name="title" class="form-control">
+    <input type="text" name="title" class="form-control" value="<?php echo $post_title; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_category">Post Category</label>
-    <input type="text" name="post_category_id" class="form-control">
+    <input type="text" name="post_category_id" class="form-control" value="<?php echo $post_category_id; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_author">Post Author</label>
-    <input type="text" name="author" class="form-control">
+    <input type="text" name="author" class="form-control" value="<?php echo $post_author; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_status">Post Status</label>
-    <input type="text" name="post_status" class="form-control">
+    <input type="text" name="post_status" class="form-control" value="<?php echo $post_status; ?>">
   </div>
 
   <div class="form-group">
@@ -44,12 +47,14 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
   <div class="form-group">
     <label for="post_tags">Post Tags</label>
-    <input type="text" name="post_tags" class="form-control">
+    <input type="text" name="post_tags" class="form-control" value="<?php echo $post_tags; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_content">Post Content</label>
-    <textarea class="form-control" name="post_content" rows="10" cols="30"></textarea>
+    <textarea class="form-control" name="post_content" rows="10" cols="30">
+      <?php echo $post_content; ?>
+    </textarea>
   </div>
 
   <div class="form-group">
