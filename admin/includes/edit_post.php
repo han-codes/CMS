2664296ae2afc3e8ac23246fdb47445e9c031aaa
Han_Codes <hanniek55@gmail.com>
2018-05-18 12:09:59 +0900

    php loop through database fields and set them in variables

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,3 +1,21 @@
+<?php
+
+$query = "SELECT * FROM posts";
+$select_posts_by_id = mysqli_query($connection, $query);
+
+while($row = mysqli_fetch_assoc($select_posts_by_id)) {
+  $post_id = $row['post_id'];
+  $post_author = $row['post_author'];
+  $post_title = $row['post_title'];
+  $post_category_id = $row['post_category_id'];
+  $post_status = $row['post_status'];
+  $post_image = $row['post_image'];
+  $post_tags = $row['post_tags'];
+  $post_comment_count = $row['post_comment_count'];
+  $post_date = $row['post_date'];
+
+ ?>
+
 <form action="" method="post" enctype="multipart/form-data">
   <div class="form-group">
     <label for="title">Post Title</label>
