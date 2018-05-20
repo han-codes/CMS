027cbea209fcaef70021e078d3257f3d370ecb85
Han_Codes <hanniek55@gmail.com>
2018-05-20 18:36:29 +0900

    dynamic category titles to our table

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,7 +2,7 @@
 if (isset($_POST['create_post'])) {
   $post_title = $_POST['title'];
   $post_author = $_POST['author'];
-  $post_category_id = $_POST['post_category_id'];
+  $post_category_id = $_POST['post_category'];
   $post_status = $_POST['post_status'];
 
   $post_image = $_FILES['image']['name'];
@@ -36,9 +36,23 @@ if (isset($_POST['create_post'])) {
   </div>
 
   <div class="form-group">
-    <label for="post_category">Post Category</label>
-    <input type="text" name="post_category_id" class="form-control">
+    <select name="post_category">
+      <?php
+      $query = "SELECT * FROM categories";
+      $select_categories = mysqli_query($connection, $query);
+      confirmQuery($select_categories);
+
+      while($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+
+        echo "<option value='{$cat_id}'>{$cat_title}</option>";
+      }
+      ?>
+
+    </select>
   </div>
+
 
   <div class="form-group">
     <label for="post_author">Post Author</label>
