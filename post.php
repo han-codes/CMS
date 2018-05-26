82008660a4110ddde8d523522b8ced5f52515885
Han_Codes <hanniek55@gmail.com>
2018-05-26 13:19:26 -0500

    add comment form and echo the user input author

--- post.php
+++ post.php
@@ -53,47 +53,74 @@
                   <!-- Closing the loop in php -->
                   <?php  } ?>
 
+                  <!-- Blog Comments -->
 
-                                  <!-- Posted Comments -->
+                  <?php
+                  if(isset($_POST['create_comment'])) {
+                    // $create_comment = $_POST['create_comment'];
+                    echo $_POST['comment_author'];
+                  }
+                   ?>
+                    <!-- Comments Form -->
+                    <div class="well">
+                      <h4>Leave a Comment:</h4>
+                      <form action="" method="post" role="form">
+                        <div class="form-group">
+                          <label for="Author">Author</label>
+                          <input type="text" name="comment_author" class="form-control" >
+                        </div>
+                        <div class="form-group">
+                          <label for="Email">Email</label>
+                          <input type="email" name="comment_email" class="form-control">
+                        </div>
 
-                                  <!-- Comment -->
-                                  <div class="media">
-                                      <a class="pull-left" href="#">
-                                          <img class="media-object" src="http://placehold.it/64x64" alt="">
-                                      </a>
-                                      <div class="media-body">
-                                          <h4 class="media-heading">Start Bootstrap
-                                              <small>August 25, 2014 at 9:30 PM</small>
-                                          </h4>
-                                          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                                      </div>
-                                  </div>
+                        <div class="form-group">
+                          <label for="Comment">Your Comment</label>
+                          <textarea class="form-control" name="comment_content" rows="3" ></textarea>
+                        </div>
+                        <button type="submit" name="create_comment" class="btn btn-primary" >Submit</button>
+                      </form>
+                    </div>
+                    <!-- Posted Comments -->
 
-                                  <!-- Comment -->
-                                  <div class="media">
-                                      <a class="pull-left" href="#">
-                                          <img class="media-object" src="http://placehold.it/64x64" alt="">
-                                      </a>
-                                      <div class="media-body">
-                                          <h4 class="media-heading">Start Bootstrap
-                                              <small>August 25, 2014 at 9:30 PM</small>
-                                          </h4>
-                                          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                                          <!-- Nested Comment -->
-                                          <div class="media">
-                                              <a class="pull-left" href="#">
-                                                  <img class="media-object" src="http://placehold.it/64x64" alt="">
-                                              </a>
-                                              <div class="media-body">
-                                                  <h4 class="media-heading">Nested Start Bootstrap
-                                                      <small>August 25, 2014 at 9:30 PM</small>
-                                                  </h4>
-                                                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                                              </div>
-                                          </div>
-                                          <!-- End Nested Comment -->
-                                      </div>
-                                  </div>
+                    <!-- Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Start Bootstrap
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+
+                    <!-- Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Start Bootstrap
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            <!-- Nested Comment -->
+                            <div class="media">
+                                <a class="pull-left" href="#">
+                                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading">Nested Start Bootstrap
+                                        <small>August 25, 2014 at 9:30 PM</small>
+                                    </h4>
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                                </div>
+                            </div>
+                            <!-- End Nested Comment -->
+                        </div>
+                    </div>
 
             </div><!-- col -->
             <!-- Blog Sidebar Widgets Column -->
